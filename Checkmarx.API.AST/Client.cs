f56f8e22ed7f9beb166c3aadfe9d79c052629394
Pedro Portilha <pedro.portilha@checkmarx.com>
2022-03-09 12:28:35 -0800

    Authentication

--- Checkmarx.API.AST/Client.cs
+++ Checkmarx.API.AST/Client.cs
@@ -1,25 +1,74 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 
 namespace Checkmarx.API.AST
 {
     public class Client
     {
+        public Uri Server { get; set; }
+
         public string Tenant { get; }
 
-        public string Username { get; set; }
+        public string KeyApi { get; set; }
 
-        public string Password { get; set; }
 
-        public Client(string tenant, string username, string password)
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public bool Connected
         {
-            if(string.IsNullOrWhiteSpace(tenant)) throw new ArgumentNullException(nameof(tenant));
-            if(string.IsNullOrWhiteSpace(username)) throw new ArgumentNullException(nameof(username));
-            if(string.IsNullOrWhiteSpace(password)) throw new ArgumentNullException(nameof(password));
-            
-            Tenant = tenant;
-            Username = username; 
-            Password = password;   
+            get
+            {
+                return Autenticate() != null;
+            }
         }
-        
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="server">
+        /// URL
+        /// https://eu.iam.checkmarx.net
+        /// US Environment - https://ast.checkmarx.net/
+        /// EU Environment - https://eu.ast.checkmarx.net/
+        /// </param>
+        /// <param name="tenant"></param>
+        /// <param name="apiKey"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Client(Uri server, string tenant, string apiKey)
+        {
+            if(server == null) throw new ArgumentNullException(nameof(server));
+            if(string.IsNullOrWhiteSpace(tenant)) throw new ArgumentNullException(nameof(tenant));
+            if(string.IsNullOrWhiteSpace(apiKey)) throw new ArgumentNullException(nameof(apiKey));
+            
+            Server = server;
+            Tenant = tenant;
+            KeyApi = apiKey; 
+        }
+
+        private string Autenticate()
+        {
+            var identityURL = $"{Server.AbsoluteUri}/auth/realms/{Tenant}/protocol/openid-connect/token";
+            var kv = new Dictionary<string, string>
+            {
+                { "grant_type", "refresh_token" },
+                { "client_id", "ast-app" },
+                { "refresh_token", $"{KeyApi}" }
+            };
+
+            var req = new HttpRequestMessage(HttpMethod.Post, identityURL) { Content = new FormUrlEncodedContent(kv) };
+            var response = _httpClient.SendAsync(req).Result;
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                JObject accessToken = JsonConvert.DeserializeObject<JObject>(response.Content.ReadAsStringAsync().Result);
+                string authToken = ((JProperty)accessToken.First).Value.ToString();
+                return authToken;
+            }
+            throw new Exception(response.Content.ReadAsStringAsync().Result);
+        }
+
     }
 }
